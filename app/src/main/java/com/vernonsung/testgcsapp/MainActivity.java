f31aafc819bad39e5db04f198b128d3aa1934c73
Vernon Sung <vernonsung@itri.org.tw>
2015-09-03 11:22:58 +0800

    Download an image from Google Cloud Storage with a hard-code URL.

--- app/src/main/java/com/vernonsung/testgcsapp/MainActivity.java
+++ app/src/main/java/com/vernonsung/testgcsapp/MainActivity.java
@@ -1,9 +1,25 @@
 package com.vernonsung.testgcsapp;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +27,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button buttonBrowseUpload = (Button)findViewById(R.id.buttonBrowseUpload);
+        buttonBrowseUpload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new DownloadTask().execute("http://testgcsserver.appspot.com.storage.googleapis.com/d45e96c0-6196-4eaa-93bc-409c5453b111.jpg");
+            }
+        });
+        ImageView imageView = (ImageView)findViewById(R.id.imageView);
+        imageView.setImageResource(R.drawable.lighthouse);
     }
 
     @Override
@@ -33,5 +58,68 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * Implementation of AsyncTask, to fetch the data in the background away from
+     * the UI thread.
+     */
+    private class DownloadTask extends AsyncTask<String, Void, Bitmap> {
+
+        @Override
+        protected Bitmap doInBackground(String... urls) {
+            try {
+                return loadFromNetwork(urls[0]);
+            } catch (IOException e) {
+                Log.e("Test", getString(R.string.connection_error));
+                return null;
+            }
+        }
+
+        /**
+         * Uses the logging framework to display the output of the fetch
+         * operation in the log fragment.
+         */
+        @Override
+        protected void onPostExecute(Bitmap bitmap) {
+            ImageView imageView = (ImageView) findViewById(R.id.imageView);
+            imageView.setImageBitmap(bitmap);
+        }
+    }
+    /** Initiates the fetch operation. */
+    private Bitmap loadFromNetwork(String urlString) throws IOException {
+        InputStream stream = null;
+        Bitmap bitmap = null;
+
+        try {
+            stream = downloadUrl(urlString);
+            bitmap =  BitmapFactory.decodeStream(stream);
+        } finally {
+            if (stream != null) {
+                stream.close();
+            }
+        }
+        return bitmap;
+    }
+
+    /**
+     * Given a string representation of a URL, sets up a connection and gets
+     * an input stream.
+     * @param urlString A string representation of a URL.
+     * @return An InputStream retrieved from a successful HttpURLConnection.
+     * @throws java.io.IOException
+     */
+    private InputStream downloadUrl(String urlString) throws IOException {
+        // BEGIN_INCLUDE(get_inputstream)
+        URL url = new URL(urlString);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setReadTimeout(10000 /* milliseconds */);
+        conn.setConnectTimeout(15000 /* milliseconds */);
+        conn.setRequestMethod("GET");
+        conn.setDoInput(true);
+        // Start the query
+        conn.connect();
+        return conn.getInputStream();
+        // END_INCLUDE(get_inputstream)
     }
 }
